55125bdeed357175d3df517bbacf505e948bacd2
Coppertalon <brogers42@my.bcit.ca>
2022-11-14 16:22:43 -0800

    can grab size of page from menu
    made sure the code will make a new list of movies each time instead of adding to same list

--- Assignment_2/index.js
+++ Assignment_2/index.js
@@ -1,7 +1,8 @@
 var pageSize = 3;
 var currentPage = 2;
 
-setup = function () {
+const display = function () {
+    $("#movies").empty();
     $.ajax({
         url: "https://api.themoviedb.org/3/movie/top_rated?api_key=749fd9ec1724adbbd9cfe5563fade508&language=en-US&page=1",
         type: "GET",
@@ -10,7 +11,7 @@ setup = function () {
             const stopIndex = startIndex + pageSize
             for (i = startIndex; i < stopIndex; i++) {
 
-                $("main").append(
+                $("#movies").append(
                     `
                     <div>
                         ${data.results[i].title}
@@ -27,6 +28,16 @@ setup = function () {
             };
         }
     });
+}
+
+setup = function () {
+    $("select").change(() => {
+        pageSize = Number($("select option:selected").val())
+        display();
+    })
+
+    display();
+
     $("body").on("click", ".img_but", function () {
         $("aside").html(
             `<img src ="https://image.tmdb.org/t/p/w500/${$(this).attr("backdropPath")}">`
