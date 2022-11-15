cee3d49485532a099fb77b05a6eeab29d55bb78b
Coppertalon <brogers42@my.bcit.ca>
2022-11-14 17:05:21 -0800

    added search function
    changed endpoint to ajax search url

--- Assignment_2/index.js
+++ Assignment_2/index.js
@@ -1,10 +1,11 @@
 var pageSize = 3;
-var currentPage = 2;
+var currentPage = 1;
+var movie_name = "up";
 
 const display = function () {
     $("#movies").empty();
     $.ajax({
-        url: "https://api.themoviedb.org/3/movie/top_rated?api_key=749fd9ec1724adbbd9cfe5563fade508&language=en-US&page=1",
+        url: `https://api.themoviedb.org/3/search/movie?api_key=749fd9ec1724adbbd9cfe5563fade508&language=en-US&page=1&include_adult=false&query=${movie_name}`,
         type: "GET",
         success: function (data) {
             const startIndex = (currentPage - 1) * pageSize
@@ -30,7 +31,45 @@ const display = function () {
     });
 }
 
+function get_movie() {
+    movie_name = $("#search_term").val()
+    console.log(movie_name)
+    currentPage = 1
+    display();
+}
+
+function set_page_num() {
+    currentPage = $(this).attr('id')
+    console.log(movie_name)
+    display();
+}
+
+function set_page_rel() {
+    let set_page = $(this).attr('id')
+    console.log(set_page)
+    if (set_page == "first") {
+        currentPage = 1
+    }
+    if (set_page == "last") {
+        currentPage = 7
+    }
+    if (set_page == "next") {
+        currentPage += 1
+        if (currentPage > 7) {
+            currentPage = 7
+        }
+    }
+    if (set_page == "previous") {
+        currentPage -= 1
+        if (currentPage < 1) {
+            currentPage = 1
+        }
+    }
+    display();
+}
+
 setup = function () {
+
     $("select").change(() => {
         pageSize = Number($("select option:selected").val())
         display();
@@ -43,6 +82,10 @@ setup = function () {
             `<img src ="https://image.tmdb.org/t/p/w500/${$(this).attr("backdropPath")}">`
         )
     })
+
+    $("#page_numbers").on("click", ".num_page_btn", set_page_num)
+    $("#search_btn").click(get_movie);
+    $("#buttons").on("click", ".rel_page_btn", set_page_rel)
 }
 
 
